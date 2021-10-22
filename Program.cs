db013b5147386e6a9ef5420d1cadacc170f32c32
Dimitrios Gkizis <gizisdimitrios@hotmail.com>
2021-10-22 14:00:50 +0300

    Random game number first commit.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RandomNumGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 20);
+            int guessNumber;
+
+            Console.WriteLine("GUESS THE NUMBER between 0 and 20!");
+            Console.WriteLine("I am thinking of a number!");
+
+            for (int guess = 0; guess < 3; guess++)
+            {
+                Console.Write("Take a guess: ");
+                guessNumber = Convert.ToInt32(Console.ReadLine());
+                if (guessNumber == randomNumber)
+                {
+                    Console.WriteLine($"YOU GOT IT! The correct number is {randomNumber}!");
+                    break;
+                }
+                else if (guessNumber < randomNumber)
+                {
+                    if (guess == 2)
+                    {
+                        Console.WriteLine($"Game Over!\nSorry. The number I was thinking was {randomNumber}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mmm... Not quite! Try a bigger number.");
+                    }
+                }
+                else
+                {
+                    if (guess == 2)
+                    {
+                        Console.WriteLine($"Game Over!\nSorry. The number I was thinking was {randomNumber}.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nope... Try a smaller number.");
+                    }
+                }
+            }
+        }
+    }
+}
